25be9e315a3688291e042dd6ebb1e4a4d037055f
Moacirguilherme <moagt@yahoo.com.br>
2025-03-06 10:54:11 -0300

    Evolucao nos estudos

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log(123)
+const list = document.querySelector('ul')
+let myLi = ''
+
+menuOptions.forEach(produt => {
+myLi = myLi + `
+    <li>
+    <img src=${produt.src}>
+    <p>${produt.name}</p>
+    <p class="item-price">${produt.price}</p>
+</li>
+`
+
+})
+list.innerHTML = myLi
+console.log(myLi)
