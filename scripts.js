06cbc8fbfbff96747c9bf7126b015c354e8a7a3f
Moacirguilherme <moagt@yahoo.com.br>
2025-03-13 06:27:10 -0300

    finalizado

--- scripts.js
+++ scripts.js
@@ -2,17 +2,27 @@ const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
 const buttonSumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filter-all')
+
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+    })
+    return newValue
+}
+
 
 
 function showAll(productsArray) {
-   let myLi = ''
+    let myLi = ''
 
     productsArray.forEach((product) => {
         myLi += `
         <li>
         <img src=${product.src}>
         <p>${product.name}</p>
-        <p class="item-price">R$ ${product.price}</p>
+        <p class="item-price">${formatCurrency(product.price)}</p>
     </li>
     `
     })
@@ -23,29 +33,32 @@ function showAll(productsArray) {
 
 function mapAllItens() {
     const newPrices = menuOptions.map((product) => ({
-...product,
-price: product.price * 0.9,
+        ...product,
+        price: product.price * 0.9,
     }))
     showAll(newPrices)
 }
 
 function sumAllItens() {
-    const totalValue = menuOptions.reduce( (acc , curr)=> acc + curr.price, 0)
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
     list.innerHTML = `
     <li>
-    <p> O Valor total dos itens é R$ ${totalValue} </p>
+    <p> O Valor total dos itens é ${formatCurrency(totalValue)} </p>
     </li>
     `
 
 }
 
+function filterAllItens() {
+    const filterJunstVegan = menuOptions.filter((produt) => produt.vegan)
+    showAll(filterJunstVegan)
+}
+
+
 buttonShowAll.addEventListener('click', () => showAll(menuOptions))
 buttonMapAll.addEventListener('click', mapAllItens)
 buttonSumAll.addEventListener('click', sumAllItens)
-
-
-
-
+filterAll.addEventListener('click', filterAllItens)
 
 
 
